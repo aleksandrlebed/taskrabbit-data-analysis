402688c83a3f8c8e658112b1cad93ac3173d0939
aleksandrlebed <aleksandr.lebed1797@gmail.com>
2026-04-15 18:07:20 -0400

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,2 +1,19 @@
+-- Total Revenue
 SELECT SUM(price) AS total_revenue
 FROM taskrabbit_raw;
+
+-- Revenue by Category
+SELECT 
+    category,
+    SUM(price) AS revenue
+FROM taskrabbit_raw
+GROUP BY category
+ORDER BY revenue DESC;
+
+-- Tasks by Category
+SELECT
+    category,
+    COUNT(*) AS total_tasks
+FROM taskrabbit_raw
+GROUP BY category
+ORDER BY total_tasks DESC;
